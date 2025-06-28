09ebe7dab215070ad769739efdc315cf9a469992
kavin1337 <113599631+kavin1337@users.noreply.github.com>
2025-06-28 11:00:22 +0530

    Create sequence_item.sv

---
+++ sequence_item.sv
@@ -0,0 +1,20 @@
+//uvm object class
+
+class dff_seq_item extends uvm_sequence_item;
+  `uvm_object_utils(dff_seq_item) // reg class to uvm factory
+  
+  rand logic rst;
+  rand logic d;
+  logic q;
+  
+  //standard constructor
+  function new(string name ="dff_seq_item");
+    super.new(name);
+    `uvm_info("sequence item Class", "constructor", UVM_MEDIUM)
+  endfunction
+  
+  //no phases
+  
+  constraint wr_rd_c { rst != 1; }; 
+ 
+endclass
